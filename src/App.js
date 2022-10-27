3c6063805d878acfe0f58efafdfd68a9b007c287
PratikGarai <pratikgarai0208@gmail.com>
2022-10-27 21:27:12 +0530

    first page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
 import "./App.css";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Avatar from '@mui/material/Avatar';
 import { useState } from "react";
 import { useEffect } from "react";
 
-const getServerDetails = async() => {
+const getServerDetails = async (handler) => {
+  let data = await fetch("http://127.0.0.1:5000/getGuilds");
+  data = await data.json();
+  handler(data.data);
+};
 
-}
-
-const getServerCard = () => {
+const getServerCard = (server) => {
+  let iconAvatar;
+  if(server.icon) {
+    iconAvatar = <Avatar alt={server.name} src={server.icon} sx={{ width: 56, height: 56 }} />
+  } else {
+    iconAvatar = <Avatar sx={{ width: 56, height: 56 }}>{server.name[0]}</Avatar>
+  }
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card elevation={3}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
+        <div className="server-icon">
+          {iconAvatar}
+        </div>
+        <Typography sx={{ fontSize: 30 }} color="text.primary" gutterBottom>
+          {server.name}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+          #{server.guild_id}
         </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography variant="body2" sx={{ fontSize: 20 }}>
+          Offenders : {server.offenders}/{server.members}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="medium">View Details</Button>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
 
 function App() {
-
-  const [servers, setServer] = useState([])
+  const [servers, setServer] = useState([]);
 
   useEffect(() => {
-    getServerDetails(setServer)
-  }, [])
+    getServerDetails(setServer);
+  }, []);
 
   return (
     <div className="App">
       <div className="header">
-        <img className="title-img" src="Pink Brush Stroke.png" alt="background"/>
+        <img
+          className="title-img"
+          src="Pink Brush Stroke.png"
+          alt="background"
+        />
         <div className="title-main">Hater</div>
         <div className="title-sub">
           The Hate Speech Detection and Tracking Discord Bot
@@ -58,7 +71,15 @@ function App() {
         <hr />
 
         <div className="body-page1-serverlist">
-          {getServerCard()}
+          <Grid sx={{ flexGrow: 1 }} container spacing={5} justifyContent="center">
+            {servers.map((server) => {
+              return (
+                <Grid item xs={4} minWidth="400px">
+                  {getServerCard(server)}
+                </Grid>
+              );
+            })}
+          </Grid>
         </div>
       </div>
     </div>
